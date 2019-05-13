83bc766c37233896b6b13e4c4d30e0718d17e32f
OGLinuk <daniel-l-black@protonmail.com>
2019-05-13 14:55:04 +1200

    Adding profile base

--- assessment2/web701/authentication/urls.py
+++ assessment2/web701/authentication/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('view_profile', views.view_profile, name='view_profile')
 ]
--- assessment2/web701/authentication/views.py
+++ assessment2/web701/authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from .forms import ProfileForm
 
 def index(request):
     return render(request, 'authentication/index.html')
@@ -8,20 +9,30 @@ def index(request):
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+            profile = profile_form.save(commit=False)
+
+            profile.user = user
+            profile.save()
+
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             
             login(request, user)
             
-            return redirect('profile')
+            return redirect('index')
 
     else:
         form = UserCreationForm()
+        profile_form = ProfileForm()
 
-    context = {'form': form}
+    context = {'form': form, 'profile_form': profile_form}
+    
     return render(request, 'registration/register.html', context)
+
+def view_profile(request):
+    return render(request, 'authentication/view_profile.html')
