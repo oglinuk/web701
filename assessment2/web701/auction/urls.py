8d246aeb78a39d7fe50b3441e8163dc5a093c8c8
OGLinuk <daniel-l-black@protonmail.com>
2019-05-22 12:42:26 +1200

    Iterating auction

--- assessment2/web701/auction/urls.py
+++ assessment2/web701/auction/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('auction', views.auction, name='auction'),
+    path('bid_item', views.bid_item, name='bid_item'),
+    path('sell_item', views.sell_item, name='sell_item')
 ]
--- assessment2/web701/auction/views.py
+++ assessment2/web701/auction/views.py
@@ -1,4 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+dummy_auction_items = {
+    "Items": [
+        {
+            "name": "strawberries",
+            "quantity": 100,
+            "price": 1.35,
+            "bidders": []
+        },
+        {
+            "name": "blueberries",
+            "quantity": 70,
+            "price": 2.10,
+            "bidders": []
+        },
+        {
+            "name": "blackberries",
+            "quantity": 130,
+            "price": 3.99,
+            "bidders": []
+        },
+        {
+            "name": "kiwiberries",
+            "quantity": 50,
+            "price": 6.82,
+            "bidders": []
+        },
+    ]
+}
 
 def auction(request):
-    return render(request, 'auction/auction.html')
+    context = {'listings': dummy_auction_items}
+    return render(request, 'auction/auction.html', context=context)
+
+def bid_item(request):
+    if request.method == 'POST':
+        form_data = request.POST.dict()
+
+        bidder = form_data.get('user')
+        quantity = int(form_data.get('quantity'))
+        listing = form_data.get('listing')
+        price = float(form_data.get('price'))
+        total_price = price * quantity
+
+        for val in dummy_auction_items.values():
+            for item in val:
+                if listing == item['name']:
+                    item['bidders'].append({
+                                        'name': bidder, 
+                                        'quantity': quantity, 
+                                        'price': total_price})
+                    item['quantity'] -= quantity
+        return redirect('auction')
+
+    return redirect('auction')
+
+def sell_item(request):
+    if request.method == 'POST':
+        form_data = request.POST.dict()
+
+        item_data = {
+            'name': form_data.get('name'),
+            'quantity': int(form_data.get('quantity')),
+            'price': float(form_data.get('price')),
+            'bidders': []
+        }
+
+        dummy_auction_items["Items"].append(item_data)
+        return redirect('auction')
+
+    return render(request, 'auction/sell.html')
